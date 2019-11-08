099cae4469a7a2c3ef4573e863df8ac43ecf1746
Pramuankosonyut <weiitasapon@WEIIMacbook.local>
2019-11-08 19:55:42 +0900

    first commit

---
+++ pages/inspire.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <img
+        src="../assets/img/Asset1.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <blockquote class="blockquote">
+        &#8220;The future of para-rubber industrials&#8221;
+        <footer>
+          <small>
+            <em>&mdash;Weii Tasapon</em>
+          </small>
+        </footer>
+      </blockquote>
+       
+  </v-card>
+      <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="./"
+          >
+            Back
+          </v-btn>
+          <v-btn
+            color="green"
+            nuxt
+            to="/About"
+          >
+            About
+          </v-btn>
+        </v-card-actions>
+    </v-flex>
+  </v-layout>
+</template>
